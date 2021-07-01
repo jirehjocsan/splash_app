a533b34d6c2d0d152ae8800272f77a2406bb7e61
jirehjocsan <jirehper_alv@hotmail.com>
2021-07-01 10:29:18 -0500

    Initial commit

---
+++ splash/splash/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace splash
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void btn_ingresar(object sender, EventArgs e)
+        {
+            if ((txt_user.Text == "Juan") && (txt_pass.Text == "123456"))
+            {
+                await DisplayAlert("Registro", "Es Correocto el usuario", "Cerrar");
+            }
+            else 
+            {
+                await DisplayAlert("Registro", "Es incorrecto el usuaio o contraseña", "Cerrar");
+            }
+        }
+    }
+}
